8f4888a40c1f679700ee376001c525c3f9c004e9
DaveGutHib <120181687+DaveGutHib@users.noreply.github.com>
2023-01-13 11:58:49 +0000

    regular commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,39 @@
 document.addEventListener("click", randomName);
 
 var names = [];
+let pairs = [];
+let revealCount = 0;
 
-//     // action for clear button
-function clearButton(evt) {
-    if (evt.target.id === "clear") {
-        [...document.querySelectorAll("input[type='text']")]
-            .forEach(input => input.id.startsWith("name") && (input.value = ""))
+function reveal() {
+    console.log("revealcount");
+    console.log(revealCount);
+    let person = pairs[revealCount];
+    console.log("Print names from reveal button");
+    console.log(pairs);
+    console.log(person["Gifter"])
+    document.getElementById("giftee"+revealCount).innerText = person.Giftee;
+    revealCount = revealCount + 1;
+    
+    g = document.createElement('div');
+    g.setAttribute("id", "gifter"+revealCount);
+    g.innerText = person.Giftee;
+    document.getElementById("results-container").appendChild(g);
 
-    }
+    g = document.createElement('div');
+    g.setAttribute("id", "giftee"+revealCount+1);
+    g.innerText = person.Giftee;
+    document.getElementById("results-container").appendChild(g);
+    
 }
 
+const shuffle = (randomiser) => {
+    for (let x = randomiser.length - 1; x > 0; x--) {
+        const y = Math.floor(Math.random() * (x + 1));
+        [randomiser[x], randomiser[y]] = [randomiser[y], randomiser[x]];
+    }
+    return randomiser;
 
-
-
+}
 
 function randomName(evt) {
     if (evt.target.id === "randomName") {
@@ -26,22 +46,17 @@ function randomName(evt) {
         // map to values only
         console.clear();
         console.log(names);
-    }
 
+        // must be given at least two names to randomise
+        if (names.length >= 2) {
+            let item = names[Math.floor(Math.random() * names.length)];
+            console.log(`Output secret Santa List: ${item}`);
+        } else {
+            console.log(`Please enter at least two names`);
+        }
 
-    // must be given at least two names to randomise
-    if (names.length >= 2) {
-        let item = names[Math.floor(Math.random() * names.length)];
-        console.log(`Output secret Santa List: ${item}`);
-    } else {
-        console.log(`Please enter at least two names`);
-    }
-}
+        //Shuffling the order of the names in the array
 
-
-//Shuffling the order of the names in the array
-function shuffler(evt) {
-    if (evt.target.id === "randomName") {
         const shuffle = (randomiser) => {
             for (let x = randomiser.length - 1; x > 0; x--) {
                 const y = Math.floor(Math.random() * (x + 1));
@@ -50,46 +65,29 @@ function shuffler(evt) {
             return randomiser;
 
         }
+
+        const randomisedNames = shuffle(names);
+
+        //Pair person 1 in randomised list with person 2, 2 with 3, and the max value back with person 1
+
+        pairs = randomisedNames.map((name, index) => {
+            return {
+                Gifter: name,
+                Giftee: randomisedNames[index + 1] || randomisedNames[0],
+            }
+        });
+        let person = pairs[revealCount];
+        document.getElementById("gifter"+revealCount).innerText = person.Gifter;
+        document.getElementById("reveal-btn").addEventListener("click", reveal);
+        console.log(pairs);
+	console.log(person.Gifter);
+
     }
-}
-function pairer(evt) {
-    const randomisedNames = shuffle(names);
+    // action for clear button
+    if (evt.target.id === "clear") {
+        [...document.querySelectorAll("input[type='text']")]
+            .forEach(input => input.id.startsWith("name") && (input.value = ""))
 
-    //Pair person 1 in randomised list with person 2, 2 with 3, and the max value back with person 1
-
-    const pairs = randomisedNames.map((name, index) => {
-        return {
-            Gifter: name,
-            Giftee: randomisedNames[index + 1] || randomisedNames[0],
-        }
-    });
-    console.log(pairs);
-    return;
-}
-function addNewName(evt) {
-    if (evt.target.id === "addNewName") {
-
-
-        const label = document.createElement("label");
-        label.setAttribute("for", "newName")
-        label.innerHTML = "Add new name";
-        document.body.appendChild(label);
-
-        const input = document.createElement("input");
-        input.setAttribute("type", "text");
-        document.body.appendChild(input);
     }
+
 }
-
-function reveal(evt) {
-    var r = document.getElementsByClassName("row");
-    if (r.style.display === "none") {
-        r.style.display = "block";
-    } else {
-        r.style.display = "none";
-    }
-}
-
-
-
-
